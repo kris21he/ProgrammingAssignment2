65a73b6b92158ab7a5ec52da8495bf46da631a62
Kris He <hjianxin@163.com>
2014-07-26 10:22:02 +0800

    new cachematrix.R
    for experience how lexical scoping in more deep level

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Creates a list containing a function to
+##  set the value of the matrix
+##  get the value of the matrix
+##  set the value of the inverse matrix
+##  get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    r <- NULL
+    set <- function(y) {
+        x <<- y
+        r <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) r <<- solve
+    getsolve <- function() r
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## 1. Try to get the inverse matrix from the cache made by makeCacheMatrix above. 
+## 2. If the inverse has already been calculated (and the matrix has not changed), return the cache value
+## 3. Otherwise, get not calculatef matrix out and culculate the inverse matrix
+## 4. Set the inverse matrix into cache for further use 
+## 5. Then return the inverse matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    r <- x$getsolve()
+    if(!is.null(r)) {
+        message("getting cached data")
+        return(r)
+    }
+    data <- x$get()
+    r <- solve(data, ...)
+    x$setsolve(r)
+    r
 }
